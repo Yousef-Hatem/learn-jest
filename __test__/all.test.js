e6607bdc27cea14c27cb409e7f967736565fbee8
Yousef Hatem <yousef26hatem@gmail.com>
2023-06-14 18:14:55 +0300

    Expect Anything And Any And Array Containing

--- __test__/all.test.js
+++ __test__/all.test.js
@@ -112,3 +112,26 @@ expect.extend({
 test("Check If Year Is Between Years Range", () => {
   expect(1982).toBeBetween(1980, 2020);
 });
+
+// expect.anything() => Anything But Undefined + Null
+
+test("Expect Anything", () => {
+  let x = "A";
+  expect("Yousef").toEqual(expect.anything());
+  expect(1).toEqual(expect.anything());
+  expect([1, 2, 3, 4]).toEqual(expect.anything());
+  expect(x).toEqual(expect.anything());
+});
+
+// expect.any(Constructor)
+
+test("Expect Any Constructor", () => {
+  expect("Yousef").toEqual(expect.any(String));
+});
+
+// expect.arrayContaining(array)
+
+test("Expect Array To Be Found In The Main Array", () => {
+  const myArray = [1, 2, 3, 4, 5, 6];
+  expect(myArray).toEqual(expect.arrayContaining([5, 3, 4, 6]));
+});
